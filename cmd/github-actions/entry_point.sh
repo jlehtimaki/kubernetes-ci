14761182537e017ce2133c1a15995a323081ff4d
Joonas Lehtimäki <joonas.lehtimaki@gmail.com>
2020-12-01 12:26:35 +0200

    Fixed entry_point.sh parameter parsing Added params to action.yml

--- cmd/github-actions/entry_point.sh
+++ cmd/github-actions/entry_point.sh
@@ -1,6 +1,23 @@
 #! /usr/bin/env bash
 
+# Convert actions to multiple actions
+opts=""
+IFS=","
+while (( "$#" )); do
+    if [[ $1 == "--actions" ]]; then
+      read -a tmp_opts <<< $2
+      for o in "${tmp_opts[@]}"; do
+        opts="$opts --action $o"
+      done
+      break
+    else
+      opts="$opts $1"
+      shift
+    fi
+done
+
+# Run the script
 if [ "$0" = "${BASH_SOURCE[*]}" ] ; then
   >&2 echo -E "\nRunning Kubernetes-CI...\n"
-  /bin/kubernetes-ci $*
+  echo $opts
 fi
