28043397af5cba9f201653c81ae65838bccc41a8
Joonas Lehtimäki <joonas.lehtimaki@gmail.com>
2020-12-03 15:50:42 +0200

    Trying to fix kubernetes-ci execute

--- cmd/github-actions/entry_point.sh
+++ cmd/github-actions/entry_point.sh
@@ -3,21 +3,35 @@
 # Convert actions to multiple actions
 opts=""
 IFS=","
-while (( "$#" )); do
-    if [[ $1 == "--actions" ]]; then
-      read -a tmp_opts <<< $2
-      for o in "${tmp_opts[@]}"; do
-        opts="$opts --actions $o"
-      done
-      break
-    else
-      opts="$opts $1"
-      shift
-    fi
-done
+parse_args() {
+  while (( "$#" )); do
+      if [[ $1 == "--actions" ]]; then
+        read -a tmp_opts <<< $2
+        for o in "${tmp_opts[@]}"; do
+          opts="$opts --actions $o"
+        done
+        break
+      else
+        opts="$opts $1"
+        shift
+      fi
+  done
+  # set remaining positional arguments (if any) in their proper place
+  eval set -- "$opts"
 
-# Run the script
-if [ "$0" = "${BASH_SOURCE[*]}" ] ; then
-  >&2 echo -E "\nRunning Kubernetes-CI...\n"
+  echo "${opts/ /}"
+  return 0
+}
+
+# Generates client.
+kubernetes-ci() {
   /bin/kubernetes-ci $opts
+}
+
+args=("$@")
+
+if [ "$0" = "${BASH_SOURCE[*]}" ] ; then
+  echo $opts
+  >&2 echo -E "\nRunning Kubernetes-CI...\n"
+  kubernetes-ci "${args[@]}"
 fi
