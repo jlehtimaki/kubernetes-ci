60d2e833a4bf69c3229277c3b67a1c6ad06f9411
Joonas Lehtimäki <joonas.lehtimaki@gmail.com>
2020-12-09 16:54:12 +0200

    add echo for TS

--- cmd/github-actions/entry_point.sh
+++ cmd/github-actions/entry_point.sh
@@ -3,35 +3,21 @@
 # Convert actions to multiple actions
 opts=""
 IFS=","
-parse_args() {
-  while (( "$#" )); do
-      if [[ $1 == "--actions" ]]; then
-        read -a tmp_opts <<< $2
-        for o in "${tmp_opts[@]}"; do
-          opts="$opts --actions $o"
-        done
-        break
-      else
-        opts="$opts $1"
-        shift
-      fi
-  done
-  # set remaining positional arguments (if any) in their proper place
-  eval set -- "$opts"
 
-  echo "${opts/ /}"
-  return 0
-}
+while (( "$#" )); do
+    if [[ $1 == "--actions" ]]; then
+      read -a tmp_opts <<< $2
+      for o in "${tmp_opts[@]}"; do
+        opts="$opts --actions $o"
+      done
+      break
+    else
+      opts="$opts $1"
+      shift
+    fi
+done
+echo $opts
 
-# Generates client.
-kubernetes-ci() {
-  /bin/kubernetes-ci $opts
-}
+COMMAND="${opts}"
 
-args=("$@")
-
-if [ "$0" = "${BASH_SOURCE[*]}" ] ; then
-  echo $opts
-  >&2 echo -E "\nRunning Kubernetes-CI...\n"
-  kubernetes-ci "${args[@]}"
-fi
+/bin/kubernetes-ci $COMMAND
